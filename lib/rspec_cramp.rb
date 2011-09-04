85623db19bc6d8ce7ffbb68bd8d4a06145557c73
Martin Bilski <martinb@Martin-Bilskis-MacBook-Air.local>
2011-09-04 10:56:33 +0200

    Mostly works, times out when trying to get("/") with a successful response followed by no body (no render in on_start).

--- lib/rspec_cramp.rb
+++ lib/rspec_cramp.rb
@@ -5,25 +5,48 @@ module Cramp
       @request = Rack::MockRequest.new(app)
     end
     
-    def get(path, options = {}, &callback)
-      async_get = proc do |path, options, callback|
-        puts "y"
+    def get(path, options = {}, &block)
+      async_request = proc do |method, path, options, callback|
         headers = {'async.callback' => callback}
-        timeout_secs = options.delete(:timeout) || 5
+        timeout_secs = options.delete(:timeout) || default_timeout
         timeout(timeout_secs) do
           EM.run do
             catch(:async) do
-              puts "eu"
-              result = @request.get(path, headers)
+              result = @request.send(method, path, headers)
               callback.call([result.status, result.header, "Something went wrong"])
             end
           end
         end
       end
-      if callback
-        async_get.call(path, options, callback)
+      
+      if block
+        async_request.call(:get, path, options, block)
       else
-        {:path => path, :options => {}, :method => async_get}
+        max_chunks = options.delete(:max_chunks) || 1
+        result = []
+        callback = lambda do |response|
+          pp response
+          puts response[-1].closed?
+          result = [response[0], response[1], []]
+          stopping = false
+          if response[-1].is_a? String
+            result[2] << response[-1]
+            stop
+          elsif response[0].between?(200, 299)
+            response[-1].each do |chunk| 
+              result[2] << chunk unless stopping
+              if result[2].count >= max_chunks
+                stopping = true
+                EM.next_tick { EM.stop }
+              end            
+            end
+          else
+            result[2] = ["Error #{response[0]}"]
+            stop
+          end
+        end
+        async_request.call(:get, path, options, callback)
+        result
       end
     end
     
@@ -31,18 +54,52 @@ module Cramp
       EM.stop
     end
     
+    def default_timeout
+      3
+    end
+    
     RSpec::Matchers.define :respond_with do |options = {}|
-      match do |request|
-        # pp request[:method]
-        @result = false
-        callback = proc do |actual_status, actual_headers, deferred_body|
-          puts "there is a response: #{actual_status}"
-          expected_status = options.delete(:status)
-          @result = expected_status.nil? || expected_status == actual_status
-          stop
+      def match?(actual, expected)
+        if expected.nil?
+          true  # No expectation set.
+        elsif actual.nil?
+          false
+        elsif expected.is_a? Regexp
+          actual.to_s.match(expected)
+        elsif expected.is_a? Integer
+          actual.to_i == expected
+        elsif expected.is_a? String
+          actual.to_s == expected
+        else
+          raise "Unsupported type"
         end
-        request[:method].call(request[:path], callback) 
-        @result
+      end
+      def resolve_status(status)
+        case status
+        when :ok then /^2[0-9][0-9]$/
+        when :error then /^[^2][0-9][0-9]$/
+        else status
+        end
+      end
+      def match_status
+        match?(@actual_status, resolve_status(@expected_status))
+      end
+      def match_headers
+        @expected_header.nil? || @expected_header.find do |ek, ev| 
+          @actual_headers.find { |ak, av| ak == ek && !match?(av, ev) } != nil
+        end == nil 
+      end
+      def match_body
+        match?(@actual_status, @expected_body)
+      end
+      match do |request_result|
+        @expected_status = options.delete(:status)
+        @actual_status = request_result[0]
+        @expected_header = options.delete(:header)
+        @actual_headers = request_result[1]
+        @expected_body = options.delete(:body)
+        @actual_body = request_result[2]
+        match_status && match_headers && match_body
       end
     end
   end
--- spec/lib/rspec_cramp_spec.rb
+++ spec/lib/rspec_cramp_spec.rb
@@ -5,37 +5,56 @@ require "http_router"
 
 module Cramp
   
-  $stdout.sync = true
-  
-  describe "rspec support for cramp" do
-    describe "respond_with", :cramp => true do
+
+    
+  describe "rspec-cramp" do
+    describe "respond_with matcher", :cramp => true do
+      
+      class SuccessfulResponse < Cramp::Action
+        def start
+          render "ok"
+          finish
+        end
+      end
+      
+      class ErrorResponse < Cramp::Action
+        def respond_with
+          [500, {'Content-Type' => 'text/html'}]
+        end
+        def start
+          finish
+        end
+      end
+      
+      class RaiseOnStart < Cramp::Action
+        def start
+          raise "an error"
+        end
+      end
+      
+      class NoResponse < Cramp::Action
+        def start
+        end
+      end
+      
+      class CustomHeaders < Cramp::Action
+        def respond_with
+          [200, {'Extra-Header' => 'ABCD', 'Another-One' => 'QWERTY'}]
+        end
+        def start
+          render "ok"
+          finish
+        end
+      end
+      
+      # Entry point
       def app
         HttpRouter.new do
-          def simple_cramp_action(&response)
-            Class.new(Cramp::Action) do
-              define_method(:start) do
-                response.call
-              end
-            end
-          end
-
-          def respond_with_cramp_action(&response)
-            Class.new(Cramp::Action) do
-              define_method(:respond_with) do
-                response.call
-              end
-            end
-          end
-
-          add('/200').to(simple_cramp_action do
-            render "ok"
-            finish
-          end)
-          add('/500').to(respond_with_cramp_action do
-            [500, {'Content-Type' => 'text/html'}]
-          end)
-          add('/no_response').to(simple_cramp_action do
-            end)
+          add('/200').to SuccessfulResponse
+          add('/500').to ErrorResponse
+          add('/no_response').to NoResponse
+          add('/custom_header').to CustomHeaders
+          add('/raise_on_start').to RaiseOnStart
         end
       end
       
@@ -44,33 +63,127 @@ module Cramp
       # get("/url") do |status, headers, body|
       # end
   
-      shared_examples_for "async_request_method" do |method|
-        it "should handle successful response" do
-          send(method, "/200", {}).should respond_with :status => 200
-        end
-        it "should handle error response" do
-          send(method, "/500", {}).should respond_with :status => 500
-          send(method, "/500", {}).should_not respond_with :status => 200
-        end
-        it "should handle sync errors from http router" do
-          send(method, "/404", {}).should respond_with :status => 404
+      shared_examples_for "async_request" do |method|
+
+        describe "timeout" do
+          it "- timeout when no response" do
+            lambda { send(method, "/no_response") }.should raise_error Timeout::Error
+          end
+          it "- allow the timeout to be defined by the user" do
+            lambda do
+              timeout(2) do
+                lambda {send(method, "/no_response", {:timeout => 1}) }.should raise_error Timeout::Error
+              end
+            end.should_not raise_error Timeout::Error
+          end
         end
         
-        it "should timeout when no response" do
-          lambda { send(method, "/no_response", {}) }.should raise_error Timeout::Error
+        # TODO Rewrite the repetitive code below using data-based spec generation.
+        
+        describe "exact match on response status" do
+          it "should match successful response" do
+            send(method, "/200").should respond_with :status => 200
+            send(method, "/200").should respond_with :status => "200"
+            send(method, "/200").should respond_with :status => :ok
+          end
+          it "should match error response" do
+            send(method, "/500").should respond_with :status => 500
+            send(method, "/500").should respond_with :status => "500"
+            send(method, "/500").should respond_with :status => :error
+            send(method, "/500").should_not respond_with :status => 200
+            send(method, "/500").should_not respond_with :status => "200"
+            send(method, "/500").should_not respond_with :status => :ok
+          end
+          it "should match non-async errors from http router" do
+            send(method, "/404").should respond_with :status => 404
+            send(method, "/404").should respond_with :status => "404"
+          end
         end
-        it "should allow the timeout to be defined by the user" do
-          lambda do
-            timeout(2) do
-              lambda {send(method, "/no_response", {:timeout => 1}) }.should raise_error Timeout::Error
-            end
-          end.should_not raise_error Timeout::Error
+
+        describe "regex match on response status" do
+          it "should match successful response" do
+            send(method, "/200").should respond_with :status => /^2.*/
+          end
+          it "should match error response" do
+            send(method, "/500").should respond_with :status => /^5.*/
+            send(method, "/500").should_not respond_with :status => /^2.*/
+          end
+          it "should match non-sync errors from http router" do
+            send(method, "/404").should respond_with :status => /^4.*/
+          end
         end
+        
+        describe "exact match on response headers" do
+          it "should match with one expected header" do
+            send(method, "/custom_header").should respond_with :header => {"Extra-Header" => "ABCD"}
+          end
+          it "should match all with two expected headers" do
+            send(method, "/custom_header").should respond_with :header => {"Extra-Header" => "ABCD", "Another-One" => "QWERTY"}
+          end
+          it "should not match if value does not match" do
+            send(method, "/custom_header").should_not respond_with :header => {"Extra-Header" => "1234"}
+          end
+          it "should not match iff both expected headers do not match" do
+            send(method, "/custom_header").should_not respond_with :header => {"Extra-Header" => "1234", "Non-Existent-One" => "QWERTY"}
+          end
+        end
+        
+        describe "regex match on response headers" do
+          it "should match with one expected header" do
+            send(method, "/custom_header").should respond_with :header => {"Extra-Header" => /^ABCD$/}
+          end
+          it "should match all with two expected headers" do
+            send(method, "/custom_header").should respond_with :header => {"Extra-Header" => /^ABCD$/, "Another-One" => /^QWERTY$/}
+          end
+          it "should not match if value does not match" do
+            send(method, "/custom_header").should_not respond_with :header => {"Extra-Header" => /^1234$/}
+          end
+          it "should not match iff both expected headers do not match" do
+            send(method, "/custom_header").should_not respond_with :header => {"Extra-Header" => /^1234$/, "Non-Existent-One" => /^QWERTY$/}
+          end
+        end
+        
+        # FIXME How to handle a situation where nothing is rendered? get reads the body...
+        
+        describe "exact match on response body" do
+          it "should match with successful response" do
+            send(method, "/200").should respond_with :body => "ok"
+            send(method, "/200").should_not respond_with :body => "wrong"
+          end
+          it "should match error response"
+          it "should match non-async response from http router"
+        end
+        describe "regex match on response body" do
+          it "should match with successful response"
+          it "should match error response"
+          it "should match non-async response from http router"
+        end          
+   
+        describe "exact match on response body chunks" do
+          it "should match with successful response"
+          it "should match error response"
+          it "should match non-async response from http router"
+        end
+        describe "regex match on response body chunks" do
+          it "should match with successful response"
+          it "should match error response"
+          it "should match non-async response from http router"
+        end
+        
+        it "should correctly handle exception in the callbacks"
+        it "should correctly handle exception raised in on_start" do
+          get("/raise_on_start").should respond_with :code => 200
+        end
+        
+        it "should support custom request headers"
+        
+        
+        it "- support passing extra headers"
 
       end
           
-      describe "GET request method" do
-        it_should_behave_like "async_request_method", :get 
+      describe "GET request" do
+        it_should_behave_like "async_request", :get 
       end
     end
   end
